ce7d4438589f75b1b77ead48da71906f7de21777
Pankaj <pankaj@colostate.edu>
2020-06-12 14:59:36 -0600

    reomoved bug of no initialization of timeStamp

--- collect.py
+++ collect.py
@@ -374,7 +374,7 @@ def collect(client, args):
             episode_ended =  collided or lane_crossed  or \
                             carla_game.is_reset(measurements.player_measurements.transform.location)
             episode_success = not (collided or lane_crossed )
-            if (currentTimeStamp-initialTimeStamp)/1000 > episode_aspects['timeout']:
+            if image_count >= NUMBER_OF_FRAMES_CAR_FLIES and (currentTimeStamp-initialTimeStamp)/1000 > episode_aspects['timeout']:
                 episode_ended = True
                 episode_success = False
 
